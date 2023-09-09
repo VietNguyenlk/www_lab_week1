c8c8637b6594e8e0cabaf707084bd41d5948e287
VietNguyenlk <viet1282002@gmai.com>
2023-09-09 14:45:50 +0700

    tao class

---
+++ src/main/java/vn/edu/iuh/fit/controllers/ControllerServlet.java
@@ -0,0 +1,21 @@
+package vn.edu.iuh.fit.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/controllerServlet")
+public class ControllerServlet extends HttpServlet {
+
+    public  void  doGet(HttpServletRequest reg, HttpServletResponse ro) throws IOException {
+
+    }
+
+    public  void doPost(HttpServletRequest reg, HttpServletResponse ro )throws IOException {
+
+    }
+
+}
